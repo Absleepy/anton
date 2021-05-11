dcbf7085e4b5e716424f83b88aa97d977f4b617e
usman <usman.muhammad@piri.ai>
2021-05-12 03:09:28 +0500

    clean up, added scroll event for moving images

--- src/pages/about/components/hero/Hero.js
+++ src/pages/about/components/hero/Hero.js
@@ -4,7 +4,7 @@ import Image from '../animated-image/Image';
 const Hero = () => {
   const [boyCount, setBoyCount] = useState(1);
   const [girlCount, setGirlCount] = useState(1);
-  const [mouseOver, setmouseOver] = useState([false, false]);
+  const [mouseOver, setmouseOver] = useState([true, true]);
   const [pos, setpos] = useState(42);
 
   useEffect(() => {
@@ -21,13 +21,13 @@ const Hero = () => {
       let mO = [...mouseOver];
       mO[0] = leave;
       setmouseOver(mO);
-      return setBoyCount((c) => (c > 3 ? 1 : c + 1));
+      return setBoyCount((c) => (c > 3 ? 2 : c + 1));
     }
 
     let mO = [...mouseOver];
     mO[1] = leave;
     setmouseOver(mO);
-    return setGirlCount((c) => (c > 3 ? 1 : c + 1));
+    return setGirlCount((c) => (c > 3 ? 2 : c + 1));
   };
 
   return (
